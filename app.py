fdf495bdc0b1c65e9bdb49946f597d95869954d5
ibam <ibamibrahim0@gmail.com>
2021-01-14 20:10:26 +0700

    Add New Model

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import os
 import pickle
 from flask import Flask, render_template, jsonify, request, make_response
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import pickle
+import re
+import string
 
 
 class Config(object):
@@ -11,9 +16,26 @@ class Config(object):
 app = Flask(__name__)
 app.config.from_object(Config)
 
+def preprocess(title, sw_remover, stemmer):
+
+    ## Case Folding (sentence lowering, remove punctuation & numbers)
+    title = title.lower()
+    title = re.sub(r'\d+', '', title)
+    title = title.translate(str.maketrans('', '', string.punctuation))
+    title = title.strip()
+    
+    ## Filtering (Stopwords removal)
+    title = sw_remover.remove(title)
+
+    ## Stemming (Change to bsaic form)
+    title = stemmer.stem(title)
+
+    return title
+
 @app.route('/')
 def index():
     return "Hello World?"
+
 
 @app.route('/predict', methods=['GET'])
 def predict():
@@ -34,6 +56,22 @@ def predict():
 def calculate_bmi(weight, height):
     return round(float(weight) / (float(height / 100) * float(height / 100)), 2)
 
+@app.route('/predictNewsTitle', methods=['GET'])
+def predict_news_title():
+    data = request.get_json()
+    title = data['title']
+    sw_remover = StopWordRemoverFactory().create_stop_word_remover()
+    stemmer = StemmerFactory().create_stemmer()
+    vectorizer = pickle.load(open("vectorizer.pickle", "rb"))
+    model = pickle.load(open("final_model.pickle", "rb"))
+    title = preprocess(title, sw_remover, stemmer)
+    title = vectorizer.transform([title])
+    predicted_label = model.predict(title)[0]
+    result = {
+        'title': data['title'],
+        'predicted_label': predicted_label 
+    }
+    return make_response(jsonify(result), 200)
 
 if __name__ == '__main__':
     app.run(debug=True)
